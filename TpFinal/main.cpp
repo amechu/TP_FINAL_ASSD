75baba6043fb2c5503bd16201c3c32e2350b2e86
MT2321 <mrodriguez@itba.edu.ar>
2020-07-15 16:04:54 -0300

    Opencv C++

---
+++ TpFinal/main.cpp
@@ -0,0 +1,9 @@
+//RECORDAR PONER EL ENVIRONMENT EN X64
+#include "motionDetection.h"
+//#include "lucasKandale.h"
+//#include "playGround.h"
+int main(void) {	
+	motionDetection();
+	//lucasKandale();
+	//capture_test();
+}
