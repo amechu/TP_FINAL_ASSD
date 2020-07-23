2802a6269c3c8118c8cd51ad1374cbf5ad608c57
MT2321 <mrodriguez@itba.edu.ar>
2020-07-23 20:00:45 -0300

    Update camshift filter

--- Python2/MaskingFilter.py
+++ Python2/MaskingFilter.py
@@ -9,8 +9,13 @@ class HistFilter:
         self.ranges = [0, 180]
         self.hist = None
         self.mask = None
+        self.bins = 64
+        self.blur_size = 0
+        self.kernel_blur_size = 0
+        self.low_pth = 230
+        self.selection = None
 
-    def compute_hist(self, src, bins=100):
+    def compute_hist(self, src, bins=64):
         """
         This function computes the color distribuition probabilty
         for a given BGR image. Often used to compute Kernel histogram
@@ -29,6 +34,7 @@ class HistFilter:
         hist_size = max(bins, 2)
         hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
         self.mask = cv.inRange(hsv, np.array((0., 60., 0.)), np.array((180., 255., 255.)))  #((0., 60., 32.))
+        # self.mask = cv.medianBlur(self.mask,15)
         hist = cv.calcHist([hsv], [0], self.mask, [hist_size], self.ranges)
         cv.normalize(hist, hist, 0, 255, cv.NORM_MINMAX)
         self.hist = hist.reshape(-1)
@@ -110,14 +116,15 @@ class HistFilter:
     def get_mask(self, src):
         hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
         # 4 Aramamos una mascara generica que solo usa el Hue y un acotado rango de S y V
-        # res = cv.medianBlur(src, 11)
+        # hsv = cv.medianBlur(hsv, 11)
         # res = cv.dilate(hsv, (11, 11))
         mask = cv.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-
         prob = cv.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
-        cv.imshow("Mascara final", prob)
+        # cv.imshow("Mascara final", prob)
         prob &= mask
-        mask2 = cv.inRange(prob, 230, 255)
+        mask2 = cv.inRange(prob, self.low_pth, 255)
+        if self.kernel_blur_size != 0:
+            mask2 = cv.medianBlur(mask2,self.kernel_blur_size)
 
 
         # res = self.apply_hist_mask(src, self.hist)
@@ -125,6 +132,24 @@ class HistFilter:
         # res = cv.medianBlur(res, 11)
         # res = cv.dilate(res, (11, 11))
         return mask2
+
+    def set_bins(self,num):
+        self.bins = num
+    def set_mask_blur(self,blur_size):
+        if blur_size%2 != 1:
+            self.blur_size =  int(blur_size)+1
+        else:
+            self.blur_size =  int(blur_size)
+
+    def set_kernel_blur(self,blur_size):
+        if blur_size%2 == 0:
+            self.kernel_blur_size = int(blur_size)+1
+        else:
+            self.kernel_blur_size = int(blur_size)
+
+    def set_low_pth(self,low_pth):
+
+        self.low_pth = low_pth
 
 class MaskingFilter:
 
@@ -186,7 +211,8 @@ class MaskingFilter:
                     self.upperThreshold[2] += np.clip(np.clip(np.int32(self.lab_mask[0, 0, :])[2] + self.LSemiAmp, 1, 255) - self.upperThreshold[2], -self.labMaxChange, self.labMaxChange)
 
             #Histogram Filter init
-            self.hist_filter.compute_hist(selection)
+            self.hist_filter.selection = selection
+            self.hist_filter.compute_hist(selection,self.hist_filter.bins)
             self.hist_filter.show_hist(self.hist_filter.hist)
 
         self.init = False
@@ -215,8 +241,9 @@ class MaskingFilter:
             b_high = np.clip(np.int32(self.lab_mask[0, 0, :])[2] + self.bSemiAmp, 1, 255)
             self.lowerThreshold = np.array([L_low, a_low, b_low])
             self.upperThreshold = np.array([L_high, a_high, b_high])
-        else:
-            return
+        elif self.mask is self.maskingType["FILTER_CSHIFT"]:
+            self.hist_filter.compute_hist(self.hist_filter.selection, self.hist_filter.bins)
+            self.hist_filter.show_hist(self.hist_filter.hist)
 
     # def change_masking_filter(self,mask_type):
     #     if mask_type ==
